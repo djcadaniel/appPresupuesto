4988b31db043aa4b7276143f643626417d4a1459
djcadaniel <djcadaniel@gmail.com>
2024-07-25 12:58:09 -0500

    add modal

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, FormEvent, useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget";
+
+export default function BudgetForm() {
+
+  const [budget, setBudget] = useState(0);
+  const {dispatch} = useBudget();
+
+  const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+    if(budget < 0){
+      alert('Escribe un monto mayor a 0 porfavor 😉')
+    }
+    setBudget(target.valueAsNumber)
+  }
+  const isValid = useMemo(()=>{
+    return isNaN(budget) || budget < 0
+  }, [budget])
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({type: 'add-budget', payload: {budget}})
+  }
+
+  return (
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Definir presupuesto
+        </label>
+        <input 
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Define tu prepuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input 
+        type="submit"
+        value='Definir presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+        disabled={isValid}
+      />
+    </form>
+  )
+}
